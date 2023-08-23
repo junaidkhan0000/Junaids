97b1db71173fe8b497046207377c6b8e49cb05ee
junaidkhan0000 <141704817+junaidkhan0000@users.noreply.github.com>
2023-08-23 15:03:46 +0530

    hello.java

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello
+  {
+    public static void main(String [] args)
+    {
+      int i;
+      for(i=2; i<=10; i++)
+        if (i%2==0)
+          System.out.print(i);
+    }
+  }
